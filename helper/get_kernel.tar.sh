ecc6930c5d48c4447706002c1f2da696b6f82e8d
hito <hito@ursa.doll.kugutsu.org>
2014-01-01 19:46:23 +0900

    added kernel.tar generator

---
+++ helper/get_kernel.tar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# common(all arch)
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-headers-3.10.25-031025_3.10.25-031025.201312201135_all.deb
+
+# i386
+wget \
+    http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-headers-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb \
+    http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb
+
+tar cf kernel_i386_current.tar \
+    linux-headers-3.10.25-031025_3.10.25-031025.201312201135_all.deb
+    linux-headers-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb \
+    linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb \
+
+# amd64
+wget \
+    http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-headers-3.10.25-031025-generic_3.10.25-031025.201312201135_amd64.deb \
+    http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_amd64.deb
+
+tar cf kernel_amd64_current.tar \
+    linux-headers-3.10.25-031025_3.10.25-031025.201312201135_all.deb \
+    linux-headers-3.10.25-031025-generic_3.10.25-031025.201312201135_amd64.deb \
+    linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_amd64.deb
