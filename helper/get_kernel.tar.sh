48dd714f4e2b17b6a38111e567ce8ec4e2c1fe63
Fumihito YOSHIDA <hito@kugutsu.org>
2014-01-08 13:41:29 +0900

    fixed invalid kernel_i386_current.tar creation

--- helper/get_kernel.tar.sh
+++ helper/get_kernel.tar.sh
@@ -9,9 +9,9 @@ wget \
     http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.25-saucy/linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb
 
 tar cf kernel_i386_current.tar \
-    linux-headers-3.10.25-031025_3.10.25-031025.201312201135_all.deb
+    linux-headers-3.10.25-031025_3.10.25-031025.201312201135_all.deb \
     linux-headers-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb \
-    linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb \
+    linux-image-3.10.25-031025-generic_3.10.25-031025.201312201135_i386.deb
 
 # amd64
 wget \
