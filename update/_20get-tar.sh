9148b3190d08e848b7b7a0780baa22e129d34db1
hito <hito@ursa.doll.kugutsu.org>
2014-02-08 17:53:14 +0900

    update default variable (12.04.3 -> 12.04.4)

--- update/_20get-tar.sh
+++ update/_20get-tar.sh
@@ -5,7 +5,7 @@ set -u
 echo ${DEBUGOUT:=/dev/null} > /dev/null
 
 echo MASTER=${MASTER:=172.20.200.20}                 > ${DEBUGOUT}
-echo TARBALLNAME=${TARBALLNAME:=ubuntu-core-12.04.3-core-amd64_current.tar.gz} > ${DEBUGOUT}
+echo TARBALLNAME=${TARBALLNAME:=ubuntu-core-12.04.4-core-amd64_current.tar.gz} > ${DEBUGOUT}
 echo COREDOWNLOADURL=${COREDOWNLOADURL:=http://${MASTER}/ubuntu-core/${TARBALLNAME}} > ${DEBUGOUT}
 echo TEMPDIR=${TEMPDIR:=`mktemp -d /tmp/$0.XXXXXXX`} > ${DEBUGOUT}
 
