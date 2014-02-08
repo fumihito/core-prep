26ada73c360dcb894a505e8bbe3c56030ac23e95
hito <hito@ursa.doll.kugutsu.org>
2014-02-08 17:25:38 +0900

    update core download script URL to 12.04.4

--- helper/get_ubuntu-core-amd64.sh
+++ helper/get_ubuntu-core-amd64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://cdimage.ubuntu.com/ubuntu-core/releases/12.04.3/release/ubuntu-core-12.04.3-core-amd64.tar.gz -O ubuntu-core-12.04.3-core-amd64_current.tar.gz
+wget http://cdimage.ubuntu.com/ubuntu-core/releases/12.04.4/release/ubuntu-core-12.04.4-core-amd64.tar.gz -O ubuntu-core-12.04.4-core-amd64_current.tar.gz
 
 
--- helper/get_ubuntu-core-armhf.sh
+++ helper/get_ubuntu-core-armhf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget http://cdimage.ubuntu.com/ubuntu-core/releases/12.04.3/release/ubuntu-core-12.04.3-core-armhf.tar.gz -O ubuntu-core-12.04.3-core-armhf_current.tar.gz
+wget http://cdimage.ubuntu.com/ubuntu-core/releases/12.04.4/release/ubuntu-core-12.04.4-core-armhf.tar.gz -O ubuntu-core-12.04.4-core-armhf_current.tar.gz
 
