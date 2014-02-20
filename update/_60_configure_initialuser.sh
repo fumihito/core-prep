88bd4cdcf7bcf2727fbe74a49cbf805c8b756c26
hito <hito@kugutsu.org>
2014-02-20 18:44:57 +0900

    fix invalid sudoers handlings

--- update/_60_configure_initialuser.sh
+++ update/_60_configure_initialuser.sh
@@ -12,5 +12,7 @@ echo DEFAULTPASS=${DEFAULTPASS:=ubuntu} > ${DEBUGOUT}
 
 chroot ${COREROOT} useradd -d /home/${DEFAULTUSER} -m -s /bin/bash ${DEFAULTUSER}
 chroot ${COREROOT} /bin/sh -c "echo ${DEFAULTUSER}:${DEFAULTPASS} | chpasswd"
-chroot ${COREROOT} addgroup ${DEFAULTUSER} adm
-chroot ${COREROOT} addgroup ${DEFAULTUSER} sudo
+chroot ${COREROOT} addgroup adm
+chroot ${COREROOT} addgroup sudo
+chroot ${COREROOT} usermod -a -G adm  ${DEFAULTUSER}
+chroot ${COREROOT} usermod -a -G sudo ${DEFAULTUSER}
